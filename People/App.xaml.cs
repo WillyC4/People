f6d6dc67674d53c9c8fb808a6fd15bdcf6672bac
Willy Corrales <willy.corrales@udla.edu.ec>
2024-06-30 18:51:39 -0500

    Final

--- People/App.xaml.cs
+++ People/App.xaml.cs
@@ -7,6 +7,7 @@ public partial class App : Application
     // TODO: Add a public static PersonRepository property
     public static PersonRepository PersonRepo { get; private set; }
 
+
     public App(PersonRepository repo)
     {
 		InitializeComponent();
